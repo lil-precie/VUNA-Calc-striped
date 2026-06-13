be9c325020615bd19e60f425b85639885e34073b
lil-precie <preciousede436@gmail.com>
2026-06-13 13:17:23 +0100

    new changes made

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Calculator route
+app.get('/calculator.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'calculator.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✓ VUNA Calculator running at http://localhost:${PORT}`);
+});
